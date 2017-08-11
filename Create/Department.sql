759f2762e947b02c7a3f805e00b889c1fb1483a3
Lisa Kim <kimlisa@uw.edu>
2017-08-10 17:10:14 -0700

    added length constraint

--- Create/Department.sql
+++ Create/Department.sql
@@ -3,5 +3,8 @@ CREATE TABLE DEPARTMENT
 	Dept_ID			INT			NOT NULL,
 	Dep_name		VARCHAR(50)		NOT NULL,
 
-	PRIMARY KEY (Dept_ID)
+	PRIMARY KEY (Dept_ID),
+	
+	CONSTRAINT ID_LENGTH
+	CHECK (length(Dept_ID) >= 1 AND length(Dept_ID) <= 4)
 );
